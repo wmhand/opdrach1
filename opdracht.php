d2aaf4283e8567ebf3c45065f7a21135ef393f80
SD Student <302677430@student.rocmondriaan.nl>
2022-09-01 10:28:28 +0200

    BEGIN PHP GEMAAKT

---
+++ opdracht.php
@@ -0,0 +1,44 @@
+<!doctype html >
+<html lang="nl">
+
+    <head>
+        <title> opdracht 1 </title>
+    </head>
+<body>
+
+<h1>hello world</h1>
+<?php
+$today = date(" l j F Y");
+echo "vandaag is het $today <br>";
+
+$day = date("z");
+echo "vandaag is het dag $day van het jaar <br>";
+
+$day1 = date("l ");
+$week = date("w");
+
+echo "$day1 is de {$week}e dag van de week <br>";
+
+$month = date("F");
+$DayMonth = date("t");
+
+echo "de maand $month heeft in totaal $DayMonth dagen in de maand";
+
+
+if ()
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+</body>
